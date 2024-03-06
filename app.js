a219c70b368db7b5b98631aa05fe52aafa10cabd
iluna007 <146373472+iluna007@users.noreply.github.com>
2024-03-06 16:40:19 +0000

    JET installed and tested

---
+++ app.js
@@ -0,0 +1,40 @@
+// One euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+}
+
+// We declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * oneEuroIs.USD;
+    // return the dollar value
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar) {
+    // Convert the given valueInDollar to yen
+    let valueInYen = valueInDollar * oneEuroIs.JPY;
+    // return the yen value
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYen) {
+    // Convert the given valueInYen to pound
+    let valueInPound = valueInYen * oneEuroIs.GBP;
+    // return the pound value
+    return valueInPound;
+}
+
+// This is my function that sums two numbers
+const sum = (a,b) => {
+    return a + b
+}
+
+// Just a console log for ourselves
+console.log(sum(7,3))
+
+// Export the function to be used on other files 
+// (similar to the keyword "export" when using webpack)
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound }
